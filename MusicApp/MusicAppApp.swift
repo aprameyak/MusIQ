51fa60efe424a0add2ae3e48b90b64c3f92f2161
Aprameya Kannan <aprameyakannan@gmail.com>
2026-01-05 14:32:52 -0500

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Aprameya Kannan on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
