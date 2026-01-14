18a0148c41785bad93b9b33b403da57c67e2b06a
Aprameya Kannan <aprameyakannan@gmail.com>
2026-01-13 23:56:40 -0500

    pipeline

--- frontend/MusicAppUITests/MusicAppUITestsLaunchTests.swift
+++ frontend/MusicAppUITests/MusicAppUITestsLaunchTests.swift
@@ -1,9 +1,9 @@
-//
-//  MusicAppUITestsLaunchTests.swift
-//  MusicAppUITests
-//
-//  Created by Aprameya Kannan on 1/5/26.
-//
+
+
+
+
+
+
 
 import XCTest
 
@@ -22,8 +22,8 @@ final class MusicAppUITestsLaunchTests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
+        
+        
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
