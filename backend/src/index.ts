8df503b4b9168e843c79b56f76562620c6cf1e1d
Aprameya Kannan <aprameyakannan@gmail.com>
2026-01-13 23:20:24 -0500

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(securityMiddleware);
